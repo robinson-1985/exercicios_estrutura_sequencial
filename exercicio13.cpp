07fcf5b24c32454a88d50683f8573f5b983765cb
Robinson <robinpaulista85@gmail.com>
2020-12-18 14:06:04 -0300

    Lista de exercícios

--- exercicio13.cpp
+++ exercicio13.cpp
@@ -3,9 +3,15 @@ e o perímetro do quadrado. (Perímetro = 4 * L; área = L ^ 2).*/
 
 #include <stdio.h>
 int main(){
+    int lado, quadrado, area_quadrado, perimetro_quadrado;
+    printf("Declare o tamanho do lado do quadrado: ");
+    scanf("%d", &lado);
 
+    perimetro_quadrado = 4 * lado;
+    printf("O perímetro do quadrado é: %d \n", perimetro_quadrado);
 
-
+    area_quadrado = lado ^ 2; 
+    printf("A área do quadrado é: %d \n", area_quadrado);
 
     getchar ();
     return 0;
